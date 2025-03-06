99ae3f7f45676f0c8de94f16b42ab6fac79bcdae
Kevin Hui <kevinhui98@gmail.com>
2025-03-06 16:50:30 -0500

    change body element function to dynamic

--- BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -82,6 +82,8 @@ fun SearchBar(
 // Step: Align your body - Alignment
 @Composable
 fun AlignYourBodyElement(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
     modifier: Modifier = Modifier
 ) {
     Column(
@@ -90,7 +92,7 @@ fun AlignYourBodyElement(
         modifier = modifier
     ) {
         Image(
-            painterResource(id = R.drawable.ab1_inversions),
+            painterResource(id = drawable),
             contentDescription = null,
             contentScale = ContentScale.Crop,
             modifier = Modifier
@@ -98,7 +100,7 @@ fun AlignYourBodyElement(
                 .clip(CircleShape)
         )
         Text(
-            stringResource(id = R.string.ab1_inversions),
+            stringResource(id = text),
             style = MaterialTheme.typography.bodyMedium,
             modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp),
         )
@@ -209,6 +211,8 @@ fun SearchBarPreview() {
 fun AlignYourBodyElementPreview() {
     MySootheTheme {
         AlignYourBodyElement(
+            drawable = R.drawable.ab1_inversions,
+            text = R.string.ab1_inversions,
             modifier = Modifier.padding(8.dp)
         )
     }
